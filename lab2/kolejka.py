ed3eb9bfd0d24dbd7cdab59a5fdcac11546f8774
Jerzy9 <zbylutkonrad@gmail.com>
2020-10-30 10:24:41 +0100

    kolejka works

--- lab2/kolejka.py
+++ lab2/kolejka.py
@@ -1,26 +1,28 @@
 class Queue:
 
     def __init__(self, size):
-        self.Queue = [0]*size
+        self.Queue = [0] * (size + 1)
         self.head = 0
-        self.tail = 0
+        self.tail = -1
         self.size = size
+        self.max_size = size
 
     def euqueue(self, obj):
         self.tail += 1
 
-        if self.tail > self.size-1:
+        if self.tail > self.size - 1:
             self.tail = 0
 
-        if self.head == self.tail:
+        if self.head == self.tail + 1 or (self.head == 0 and self.tail == self.size - 1):
             self.tail -= 1
             print("Kolejka jest pełna")
-            pass
+            return 0
 
+        print("przypisz", obj, " na index: ", self.tail)
         self.Queue[self.tail] = obj
 
     def dequeue(self):
-        if self.tail == self.head:
+        if self.tail+1 == self.head:
             print("Pusta kolejka")
             return 0
 
@@ -28,14 +30,14 @@ class Queue:
         self.Queue[self.head] = 0
 
         self.head += 1
-        if self.head > self.size-1:
+        if self.head > self.size - 1:
             self.head = 0
 
         return head
 
 
 def main():
-    q = Queue(10)
+    q = Queue(5)
     print(q.dequeue())
     print(q.Queue)
     print(q.dequeue())
--- lab2/kolejka_z_priorytetem.py
+++ lab2/kolejka_z_priorytetem.py
@@ -1,10 +1,11 @@
-class Queue:
+class QueueP:
 
     def __init__(self, size):
-        self.Queue = [0]*size
+        self.Queue = [0]*(size+1)
         self.head = 0
-        self.tail = 0
+        self.tail = -1
         self.size = size
+        self.max_size = size
 
     def euqueue(self, obj):
         self.tail += 1
@@ -12,23 +13,16 @@ class Queue:
         if self.tail > self.size-1:
             self.tail = 0
 
-        if self.head == self.tail:
+        if self.head == self.tail+1 or (self.head == 0 and self.tail == self.size-1):
             self.tail -= 1
             print("Kolejka jest pełna")
-            pass
+            return 0
 
+        print("przypisz", obj, " na index: ", self.tail)
         self.Queue[self.tail] = obj
 
-        pre = self.tail
-        while pre-1 > 0 and self.Queue[pre] < self.Queue[pre-1]:
-            temp = self.Queue[pre]
-            self.Queue[pre] = self.Queue[pre-1]
-            self.Queue[pre-1] = temp
-
-            pre -= 1
-
     def dequeue(self):
-        if self.tail == self.head:
+        if self.tail+1 == self.head:
             print("Pusta kolejka")
             return 0
 
@@ -41,22 +35,31 @@ class Queue:
 
         return head
 
+        # if pre != 0:
+        #     print("head: ", self.head, "   tail: ", self.tail )
+        #     while pre-1 > 0 and self.Queue[pre] < self.Queue[pre-1]:
+        #         temp = self.Queue[pre]
+        #         self.Queue[pre] = self.Queue[pre-1]
+        #         self.Queue[pre-1] = temp
+        #
+        #         pre -= 1
+
 
 def main():
-    q = Queue(10)
-    print(q.dequeue())
+    q = QueueP(5)
+    q.euqueue(1); q.euqueue(2); q.euqueue(3); q.euqueue(4); q.euqueue(5);
     print(q.Queue)
-    print(q.dequeue())
+    q.dequeue()
+    q.dequeue()
+    q.dequeue()
     print(q.Queue)
-
-    q.euqueue(7)
-    q.euqueue(8)
-    q.euqueue(54)
-    q.euqueue(12)
+    q.euqueue(1);
+    q.euqueue(2);
+    q.euqueue(3);
+    q.euqueue(4);
+    q.euqueue(5);
     print(q.Queue)
-    print(q.head)
-    print(q.dequeue())
-    print(q.Queue)
+    print()
 
 
 main()
