e1a85121b3cdfe05f153b333f2d974c9ce78e3f7
Jerzy9 <zbylutkonrad@gmail.com>
2020-10-30 14:37:24 +0100

    kolejka priorytetowa

--- lab2/kolejka_z_priorytetem.py
+++ lab2/kolejka_z_priorytetem.py
@@ -4,10 +4,10 @@ class QueueP:
         self.Queue = [0]*(size+1)
         self.head = 0
         self.tail = -1
-        self.size = size
+        self.size = size+1
         self.max_size = size
 
-    def euqueue(self, obj):
+    def enqueue(self, obj):
         self.tail += 1
 
         if self.tail > self.size-1:
@@ -18,8 +18,32 @@ class QueueP:
             print("Kolejka jest pełna")
             return 0
 
-        print("przypisz", obj, " na index: ", self.tail)
         self.Queue[self.tail] = obj
+
+        pre = self.tail
+
+        while True:
+
+            if self.Queue[pre] == 0 or self.Queue[pre-1] == 0:
+                break
+
+            elif pre == 0:
+                if self.Queue[pre] > self.Queue[self.size-1] != 0:
+                    temp = self.Queue[pre]
+                    self.Queue[pre] = self.Queue[self.size-1]
+                    self.Queue[self.size-1] = temp
+
+                    pre = self.size-1
+                    continue
+
+            elif self.Queue[pre] > self.Queue[pre - 1] != 0:
+                temp = self.Queue[pre]
+                self.Queue[pre] = self.Queue[pre - 1]
+                self.Queue[pre - 1] = temp
+
+                pre -= 1
+            else:
+                break
 
     def dequeue(self):
         if self.tail+1 == self.head:
@@ -35,32 +59,18 @@ class QueueP:
 
         return head
 
-        # if pre != 0:
-        #     print("head: ", self.head, "   tail: ", self.tail )
-        #     while pre-1 > 0 and self.Queue[pre] < self.Queue[pre-1]:
-        #         temp = self.Queue[pre]
-        #         self.Queue[pre] = self.Queue[pre-1]
-        #         self.Queue[pre-1] = temp
-        #
-        #         pre -= 1
-
 
 def main():
     q = QueueP(5)
-    q.euqueue(1); q.euqueue(2); q.euqueue(3); q.euqueue(4); q.euqueue(5);
+    q.enqueue(1); q.enqueue(2);q.enqueue(3); q.enqueue(1); q.enqueue(2);q.enqueue(3);
     print(q.Queue)
     q.dequeue()
     q.dequeue()
     q.dequeue()
+    q.enqueue(8)
+    q.enqueue(9)
+    q.dequeue()
     print(q.Queue)
-    q.euqueue(1);
-    q.euqueue(2);
-    q.euqueue(3);
-    q.euqueue(4);
-    q.euqueue(5);
-    print(q.Queue)
-    print()
-
 
 main()
 
