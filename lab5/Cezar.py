60ae99aae360eaa39d78bd34c3050d7924cea73c
Jerzy9 <zbylutkonrad@gmail.com>
2020-12-05 11:22:07 +0100

    cezar done

--- lab5/Cezar.py
+++ lab5/Cezar.py
@@ -1,6 +1,10 @@
 
 def cezar(input, shift):
     output = ""
-    for i in input;
-        output
-    
+    for i in input:
+        output += (chr(ord(i) + shift))
+
+    print(output)
+
+
+cezar("ale ma kota a kot ma ale", 2)
